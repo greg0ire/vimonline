a96885ffe8f826b88c2d5294b9328399a5fd582a
scrott <scrott>
2003-02-09 18:50:26 +0000

    v3 structure

---
+++ htdocs/news/news.php
@@ -0,0 +1,48 @@
+<?php
+// news overview page
+require_once("../include/init.inc");
+require_once("../include/string_utils.inc");
+
+$page_title = "news";
+$nav_main = "news";
+$nav_sub = "news";
+$page_sid = "sid_news.php";
+$page_con = "con_news.php";
+
+include("../header.php");
+?>
+<?php
+// news overview page
+
+require("include/news.inc");
+?>
+
+<h1>Vim News</h1>
+
+<?php
+$news = getRecentNewsItems(10);
+echo mysql_error();
+for($i=0;$i<sizeof($news);$i++){
+    $news_data = $news[$i];
+?>
+
+<table cellpadding="3" cellspacing="0" border="0" width="100%">
+<tr>
+    <td class="lightbg"><span class="txth1">&nbsp;&nbsp;<?=$news_data{"title"}?></span></td>
+</tr>
+</table>
+
+<p>
+<span class="newsdate">[<?=formatNewsDate($news_data{"creation_date"})?>]</span> 
+<?=$news_data{"news"}?>
+ (<i><?=$news_data{"first_name"}?> <?=$news_data{"last_name"}?></i>) 
+<br clear="all">
+</p>
+
+<?php
+}
+?>
+&nbsp;
+<?php
+include("../footer.php");
+?>
---
+++ htdocs/news/sid_news.php
@@ -0,0 +1,5 @@
+<span class="sidebarheader">Submitting News</span>
+<p>
+Adding a news item can only be done by the few people that maintain vim-online.
+If you have something that you are sure is worth adding, send a message to Bram
+or Scott Johnston.
